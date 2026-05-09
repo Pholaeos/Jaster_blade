ebc361d91fbd30452f50e0f7da380197455574d7
Pholaeos <Pholaeos@gmail.com>
2026-05-09 16:25:14 +0200

    AJOUT => index.js => création des parties twitch pour le pc et mobile / tablettes

---
+++ js/index.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    twitch_embed_other();
+    twitch_embed_pc();
+});
+
+function twitch_embed_other() {
+    var embed = new Twitch.Embed("twitch_embed_other", {
+        width: 400,
+        height: 300,
+        channel: "jaster__blade",
+        layout: "video",
+        autoplay: false,
+    });
+
+    embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+        var player = embed.getPlayer();
+        player.play();
+    });
+}
+
+function twitch_embed_pc() {
+    var embed = new Twitch.Embed("twitch_embed_pc", {
+        width: 1189, // 16:9 aspect ratio => 889 = 16/9 * 500 et rajout espace pour le chat
+        height: 500,
+        channel: "jaster__blade",
+        // layout: "video",
+        autoplay: false,
+    });
+
+    embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+        var player = embed.getPlayer();
+        player.play();
+    });
+}
