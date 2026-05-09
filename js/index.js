163ffc4c639b055c8bb89b7c3184d1b06fad9aa3
Pholaeos <Pholaeos@gmail.com>
2026-05-09 16:44:57 +0200

    MAJ => index.js => ajout parent dans l'embed de twitch pour git mais à changer plus tard pour l'hébergement

--- js/index.js
+++ js/index.js
@@ -4,12 +4,15 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function twitch_embed_other() {
+    // TODO: changer le parent pour que ça marche sur d'autres sites
     var embed = new Twitch.Embed("twitch_embed_other", {
         width: 400,
         height: 300,
         channel: "jaster__blade",
         layout: "video",
         autoplay: false,
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["pholaeos.github.io"]
     });
 
     embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
@@ -19,12 +22,15 @@ function twitch_embed_other() {
 }
 
 function twitch_embed_pc() {
+    // TODO: changer le parent pour que ça marche sur d'autres sites
     var embed = new Twitch.Embed("twitch_embed_pc", {
         width: 1189, // 16:9 aspect ratio => 889 = 16/9 * 500 et rajout espace pour le chat
         height: 500,
         channel: "jaster__blade",
         // layout: "video",
         autoplay: false,
+        // Only needed if this page is going to be embedded on other websites
+        parent: ["pholaeos.github.io"]
     });
 
     embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
